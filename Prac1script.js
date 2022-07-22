c12c262171c805b7f149e4939b4bcc834b3add0a
MD KAVIUL HOSSAIN <kaviuln@gmail.com>
2022-07-22 18:18:44 +1000

    SIT725 Practical 1

---
+++ Prac1script.js
@@ -0,0 +1,8 @@
+var button = document.querySelector('button')
+button.onclick = function showTime() 
+{
+document.getElementById('currentTime').innerHTML = new Date().toString();
+setInterval(function () {
+	showTime();
+}, 500);}
+
